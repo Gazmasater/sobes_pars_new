88d2738c676255b7afca2956129b34ec8fb65a2c
gaz358 <tolkoplus@gmail.com>
2024-09-02 00:47:21 +0300

    galco-1вывод брэндов

--- undetected_script.py
+++ undetected_script.py
@@ -1,25 +1,30 @@
 import undetected_chromedriver as uc
 from selenium.webdriver.chrome.options import Options
-import time 
+import time
 
 # Настройки Chrome
 options = Options()
 options.binary_location = '/usr/bin/google-chrome'
-# options.add_argument('--headless')  # Если хотите запускать браузер в фоновом режиме
 
 # Указание пути к ChromeDriver вручную
 driver_path = '/home/gaz358/mygo/sobes_pars_new/chromedriver'
 
-# Создание экземпляра драйвера с указанием пути к драйверу
 try:
     driver = uc.Chrome(driver_executable_path=driver_path, options=options)
-    driver.get('https://www.galco.com')
-    print(driver.title)
-
-    time.sleep(500)
-
+    driver.get('https://www.galco.com/manufacturer')
+    print("Title:", driver.title)
+    print("Current URL:", driver.current_url)
+    
+    time.sleep(60)  # Увеличьте время ожидания, если нужно
+    
+    # Получаем HTML-код страницы
+    page_source = driver.page_source
+    
+    # Запись HTML-кода в файл
+    with open('page_source.html', 'w') as file:
+        file.write(page_source)
+    print("HTML code written to page_source.html")
   
-
 finally:
     if 'driver' in locals():
         driver.quit()
